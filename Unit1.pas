81b6e2c87304a05bbd3a43deb99e9361df0736af
etangenberg <etangenberg@hotmail.com>
2015-09-28 14:47:53 +0200

    Added Feature 1

--- Unit1.pas
+++ Unit1.pas
@@ -4,10 +4,17 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
 
 type
   TForm1 = class(TForm)
+    Label1: TLabel;
+    Label2: TLabel;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Button1: TButton;
+    Memo1: TMemo;
+    procedure Button1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -20,5 +27,13 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+ text : string;
+begin
+  text := Edit1.Text +  Edit2.Text;
+  Memo1.Lines.Append(text);
+end;
 
 end.
